27153416341835cde77e403162c94989c0c5d9b1
tguillaume59 <tguillaume59@gmail.com>
2017-03-28 10:54:31 +0200

    create project

---
+++ FindMe/app/src/main/java/com/tguillaume/findme/FDMMainActivity.java
@@ -0,0 +1,13 @@
+package com.tguillaume.findme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FDMMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
